5ed06cd3653ad42a70fc200ef473ef325ede8537
Sanjeev Jikamade <sanjeev.jikamade@gmail.com>
2018-10-20 20:22:09 +0530

    Updated app Icon, added splash screen.

---
+++ app/src/main/java/com/dark/matter/railapp/SplashAcitivity.java
@@ -0,0 +1,31 @@
+package com.dark.matter.railapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_acitivyty);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashAcitivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+
+
+}
